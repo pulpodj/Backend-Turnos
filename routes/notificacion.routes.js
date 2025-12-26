4dbbc1f51dd58abc05f534db9ff630ac1312eb72
Nico Muller <muller.nic@gmail.com>
2025-12-26 16:25:06 -0300

    Se quito el token de notificacion

--- routes/notificacion.routes.js
+++ routes/notificacion.routes.js
@@ -6,7 +6,7 @@ const fixedToken = require('../middleware/fixedToken');
 
 
 //Agregar un turno
-routes.post("/enviarMail",fixedToken,notificacionController.postMailNotificacion);
+routes.post("/enviarMail",notificacionController.postMailNotificacion);
 
 
 
